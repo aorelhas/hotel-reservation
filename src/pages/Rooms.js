15ce99ded0751a620aceaf89c213b48ad0c41376
Carlos Orelhas <c.orelhas@gmail.com>
2020-07-13 18:01:47 +0100

    Route pages/components

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 export const Rooms = () => {
-  return <div>Rooms</div>;
+  return <Hero hero="roomsHero" />;
 };
 
 export default Rooms;
