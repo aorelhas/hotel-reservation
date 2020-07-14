58aaf353c078d547de9ba044d0b7d18999110851
Carlos Orelhas <c.orelhas@gmail.com>
2020-07-14 11:57:50 +0100

    Start working in context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { RoomProvider } from './Context';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
